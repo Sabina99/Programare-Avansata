ed96e8a349bd9142309b3fa6ecbc1c19381535b6
Sabina Apostol <sabina.bubu@yahoo.com>
2020-03-27 20:17:23 +0200

    commit #1

---
+++ com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new ConfigurationPanel();
+    }
+}
